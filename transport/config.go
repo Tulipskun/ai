83a9f3fed4b2a36a086aed021cf23152fe500575
Tulipskun <103128996+Tulipskun@users.noreply.github.com>
2026-09-02 19:30:28 +0700

    feat: allow transports to be independently enabled

--- transport/config.go
+++ transport/config.go
@@ -1,19 +1,16 @@
 package transport
 
 import (
-	"errors"
 	"os"
 	"strings"
 )
 
 type Config struct {
-	DiscordToken string
+	DiscordToken   string
+	DiscordEnabled bool
 }
 
 func LoadConfig() (Config, error) {
-	config := Config{DiscordToken: strings.TrimSpace(os.Getenv("DISCORD_BOT_TOKEN"))}
-	if config.DiscordToken == "" {
-		return Config{}, errors.New("transport: DISCORD_BOT_TOKEN is required")
-	}
-	return config, nil
+	token := strings.TrimSpace(os.Getenv("DISCORD_BOT_TOKEN"))
+	return Config{DiscordToken: token, DiscordEnabled: token != ""}, nil
 }

78d87ab2f69346778f1f0c48a1761b14c3e9f6ea
Tulipskun <103128996+Tulipskun@users.noreply.github.com>
2026-09-05 20:44:57 +0700

    test: cover CLI transport configuration

--- transport/config_test.go
+++ transport/config_test.go
@@ -5,23 +5,38 @@ import "testing"
 func TestLoadConfigDisablesDiscordWithoutToken(t *testing.T) {
 	t.Setenv("DISCORD_BOT_TOKEN", "")
 	t.Setenv("DISCORD_OWNER_ID", "")
+	t.Setenv("AI_CLI_ENABLED", "")
 	config, err := LoadConfig()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if config.DiscordEnabled {
-		t.Fatal("Discord is enabled without a token")
+	if config.DiscordEnabled || config.CLIEnabled {
+		t.Fatal("no transport should be enabled")
 	}
 }
 
 func TestLoadConfigReadsDiscordToken(t *testing.T) {
 	t.Setenv("DISCORD_BOT_TOKEN", "test-token")
 	t.Setenv("DISCORD_OWNER_ID", "123456789")
+	t.Setenv("AI_CLI_ENABLED", "")
 	config, err := LoadConfig()
 	if err != nil {
 		t.Fatal(err)
 	}
 	if !config.DiscordEnabled || config.DiscordToken != "test-token" || config.DiscordOwnerID != "123456789" {
+		t.Fatalf("unexpected config: %+v", config)
+	}
+}
+
+func TestLoadConfigEnablesCLI(t *testing.T) {
+	t.Setenv("DISCORD_BOT_TOKEN", "")
+	t.Setenv("DISCORD_OWNER_ID", "")
+	t.Setenv("AI_CLI_ENABLED", "true")
+	config, err := LoadConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !config.CLIEnabled || config.DiscordEnabled {
 		t.Fatalf("unexpected config: %+v", config)
 	}
 }

7f85f0117e510f4ef2d7c4e6a1b755cde699d1e6
Tulipskun <103128996+Tulipskun@users.noreply.github.com>
2026-09-21 23:09:01 +0700

    Add persistent SessionStore abstraction

---
+++ sdk/session_store.go
@@ -0,0 +1,13 @@
+package sdk
+
+type SessionStore interface {
+	SaveSession(SessionConfig) error
+	LoadSession(string) (SessionConfig, error)
+	LoadUsage(string) (Usage, error)
+	LoadHistory(string) ([]Turn, error)
+	AppendTurns(string, []Turn, int) error
+	ReplaceTurns(string, []Turn) error
+	RecordRequest(string, int, Request) (int64, error)
+	RecordResponse(int64, Response, error) error
+	Close() error
+}
